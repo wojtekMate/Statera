a2bf93de159f2aa8ec0c6a13ff9adaa28c57fc25
wojtekMate <wojtasiak.mateo@gmail.com>
2024-03-03 11:20:04 +0100

    Add redux store

---
+++ StateraUI/statera/src/store/hooks.ts
@@ -0,0 +1,6 @@
+import { useDispatch, TypedUseSelectorHook, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from './store';
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
